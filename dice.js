de8a64b24dc103c0f08634728294b45cae9e5345
Naveeth Khan J <111632946+Naveeth97@users.noreply.github.com>
2024-10-14 15:10:23 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,25 @@
+function dicerool(){
+    var randomNumber1 =  Math.floor((Math.random() * 6) + 1);
+    var randomNumber2 =Math.floor( (Math.random() * 6) + 1);
+
+    var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
+    var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+    document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
+    document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+    
+    console.log(randomNumber1, randomNumber2);
+    console.log(randomNumber1 == randomNumber2);
+
+    if (randomNumber1 === randomNumber2){
+        document.querySelector("h1").innerHTML = "Draw";
+    } else if (randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins 🚩";
+    } else{
+        document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
+    }
+}
+
+btn = document.getElementById("container");
+
+btn.addEventListener("click", dicerool);
